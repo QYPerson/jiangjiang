4a3abfeae82ea3b241c1b091c8525e3facd53dba
ZiBin <ZiBin_1688@126.com>
2016-09-26 11:27:04 +0800

    创建酱酱助手

---
+++ 酱酱助手/酱酱助手/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  酱酱助手
+//
+//  Created by zibin on 16/9/26.
+//  Copyright © 2016年 zibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
